9eedb6801f6ac454fae4500283625c5553b8d0e5
Metche216 <drmatiasetcheverry@gmail.com>
2024-08-27 21:48:08 -0400

    Create views and templates. missing ending of part 3

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include 
 
-#path localhost:8000/polls/
+from . import views
+#path localhost:8000/polls
 
 urlpatterns = [
     path('polls/', include('polls.urls')), #MATCHED
-    path('admin/', admin.site.urls),   
+    path('admin/', admin.site.urls),
+    path('', views.home)
     
 ]
--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 
-#/
+#/polls/34/
 urlpatterns = [
     path('', views.index, name="index"),#MATCH
-    path('sample/', views.sample)
+    
 ]
